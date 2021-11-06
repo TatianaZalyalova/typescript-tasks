11cd0a37d06d59c8202fb1cbd7391073053723d1
TatianaZalyalova <39691436+TatianaZalyalova@users.noreply.github.com>
2021-11-06 15:12:31 +0300

    wrote functions spiral and semverSort

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,65 @@ const s3 = sum(3);
 alert(s3(5)); // 8
 alert(s3(6)); // 9*/
 
+export function spiral(arr: number[][]) {
+  let concatArr: number[] = [];
+  while (arr.length !== 0) {
+    concatArr = concatArr.concat(arr.shift() as number[]);
+    for (let i = 0; i < arr.length; i++) {
+      concatArr.push(arr[i].pop() as number);
+    }
+    if (arr.length !== 0) {
+      const arrDown = arr.pop() as number[];
+      concatArr = concatArr.concat(arrDown.reverse() as number[]);
+      for (let i = arr.length - 1; i >= 0; i--) {
+        concatArr.push(arr[i].shift() as number);
+      }
+    }
+  }
+  return concatArr;
+}
+
+function compareNumeric(a: string[], b: string[]): number {
+  const length = a.length > b.length ? a.length : b.length;
+  for (let i = 0; i < length; i++) {
+    const na = Number(a[i]);
+    const nb = Number(b[i]);
+    if (na > nb) return 1;
+    if (nb > na) return -1;
+    if (!Number.isNaN(na) && Number.isNaN(nb)) return 1;
+    if (Number.isNaN(na) && !Number.isNaN(nb)) return -1;
+  }
+  return 1;
+}
+
+export function semverSort(arr: string[]): string[] {
+  const arrVersion = arr.map((item) => item.split("."));
+  const arrVersionSort = arrVersion.sort(compareNumeric);
+  return arrVersionSort.map((item) => item.join("."));
+}
+
+/*class Parallel  {
+  constructor(a:number) {
+
+  }
+
+  jobs(...funcs:any[]) {
+    const arrStreams:[] = []
+    for(let i = 0; i < funcs.length; i++) {
+      funcs[i]().then(function(result:number){
+        arrStreams.push(result);
+      });
+    }
+    return arrStreams;
+  }
+}
+const runner = new Parallel(2);
+
+console.log(runner
+  .jobs(
+    () => new Promise((resolve) => setTimeout(resolve, 10, 1)),
+    () => new Promise((resolve) => setTimeout(resolve, 50, 2)),
+    () => new Promise((resolve) => setTimeout(resolve, 20, 3)),
+    () => new Promise((resolve) => setTimeout(resolve, 90, 4)),
+    () => new Promise((resolve) => setTimeout(resolve, 30, 5)),
+  )) // [1, 3, 2, 4, 5];*/
